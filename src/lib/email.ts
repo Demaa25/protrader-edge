c3153a58a9c4c30d617609463512c90ed7c80fcd
Chibuikem Nwaiwu <chibuikemnwaiwu@gmail.com>
2026-03-14 15:20:40 +0100

    Responsive commit

--- src/lib/email.ts
+++ src/lib/email.ts
@@ -3,7 +3,7 @@ import nodemailer from 'nodemailer';
 
 export const transporter = nodemailer.createTransport({
     host: process.env.SMTP_HOST,
-    port: parseInt(process.env.SMTP_PORT),
+    port: parseInt(process.env.SMTP_PORT || "465"),
     secure: true,
     auth: {
         user: process.env.SMTP_USER,
